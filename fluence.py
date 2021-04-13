63b7f1d23502adf35ba69bfc6865f87d5fab6363
Apostolos <cotothearcher@gmail.com>
2021-04-13 08:15:32 +0300

    Update with FRBSTATS data

--- fluence.py
+++ fluence.py
@@ -1,42 +1,82 @@
-import numpy as np
+from csv import reader
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-# Set MPL plot parameters
-plt.rcParams["figure.figsize"] = (25,20)
-plt.rcParams.update({'font.size': 24})
+### Set MPL plot parameters
+# Selectable SVG text
+plt.rcParams['svg.fonttype'] = 'none'
 
-# Define headers and null
-headers = ['rmp_dm', 'rmp_width', 'rmp_flux', 'null']
+# Use TeX
+plt.rcParams['text.usetex'] = True
 
+# Set figsize
+plt.rcParams["figure.figsize"] = (26,20)
+plt.rcParams["figure.dpi"] = 300
+
+# Set xtick size
+plt.rcParams['xtick.major.size'] = 20
+plt.rcParams['xtick.major.width'] = 2
+plt.rcParams['xtick.minor.size'] = 10
+plt.rcParams['xtick.minor.width'] = 2
+
+# Set ytick size
+plt.rcParams['ytick.major.size'] = 20
+plt.rcParams['ytick.major.width'] = 2
+plt.rcParams['ytick.minor.size'] = 10
+plt.rcParams['ytick.minor.width'] = 2
+
+### Load data
+# Initiate empty parameter lists
 dm = []
-width = []
 flux = []
+width = []
 
-# Load FRBCAT database
-with open('frbcat.csv') as frb_db:
-    for line in frb_db:
-        # Strip headers and nulls
-        if not any(headers in line for headers in headers):
-            # Strip quotes
-            line = line.replace('"', '')
+# Read FRBSTATS CSV catalogue
+with open('catalogue.csv', 'r') as read_obj:
+	csv_reader = reader(read_obj)
+	header = next(csv_reader)
+	# Skip header
+	if header != None:
+		for row in csv_reader:
+			dm.append(row[9])
+			flux.append(row[10])
+			width.append(row[11])
 
-            # Store FRB parameters
-            dm.append(line.split(',')[0].partition('&')[0])
-            width.append(line.split(',')[1].partition('&')[0])
-            flux.append(line.split(',')[2].partition('&')[0])
+### Pre-process data
+# Pick out incompatible rows
+idx_mask = set()
+for idx, val in enumerate(dm):
+        try:
+                dm[idx] = float(val)
+        except ValueError:
+                idx_mask.add(idx)
 
-# Convert to floats
-dm = [float(i) for i in dm]
-width = [float(i) for i in width]
-flux = [float(i) for i in flux]
+for idx, val in enumerate(flux):
+	try:
+		flux[idx] = float(val)
+	except ValueError:
+		idx_mask.add(idx)
 
-# Scatter Plot
-fig, ax = plt.subplots()
+for idx, val in enumerate(width):
+        try:
+                width[idx] = float(val)
+        except ValueError:
+                idx_mask.add(idx)
 
+# Dump rows with missing data
+for idx in sorted(idx_mask, reverse=True):
+	del dm[idx]
+	del flux[idx]
+	del width[idx]
+
+### Initiate plot
+# Apply grid
+plt.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.9)
+
+### HeRTA
 # Set minimum detectable FRB fluence (boresight)
-F_min_bs = 11.5090182
+F_min_bs = 8.056312742 # Threshold: S:N > 7
 point1_bs = [F_min_bs*1e10, 1*1e-10]
 point2_bs = [1*1e-10, F_min_bs*1e10]
 
@@ -54,37 +94,54 @@ x_hpbw = [point1_hpbw[0], point2_hpbw[0]]
 y_hpbw = [point1_hpbw[1], point2_hpbw[1]]
 
 # Plot fluence detection limits
-ax.plot(x_bs, y_bs, c='#ff7f0e', lw=4, label='Boresight (11.5 Jy ms)')
-ax.plot(x_hpbw, y_hpbw, c='#2ca02c', lw=4, label='FWHM (23 Jy ms)')
+plt.plot(x_bs, y_bs, c='#ff7f0e', lw=4, label=r'$\mathrm{Boresight \ (11.5 \ Jy \ ms)}$', alpha=0.7)
+plt.plot(x_hpbw, y_hpbw, c='#2ca02c', lw=4, label=r'$\mathrm{FWHM \ (23 \ Jy \ ms)}$', alpha=0.7)
+plt.fill_between(x_hpbw, y_hpbw, y2=1000, facecolor='green', alpha=0.2)
+plt.fill_between(x_bs, y_bs, y2=1000, facecolor='orange', alpha=0.1)
+legend = plt.legend(fontsize=37)
+legend.get_frame().set_alpha(None)
+legend.get_frame().set_facecolor((1, 1, 1, 0.7))
 
-ax.fill_between(x_hpbw, y_hpbw, y2=1000, facecolor='green', alpha=0.2)
-ax.fill_between(x_bs, y_bs, y2=1000, facecolor='orange', alpha=0.1)
+plt.annotate(r'$\mathrm{Threshold} \colon \ \mathrm{S}/\mathrm{N} \geq 7$', xy=(1005, 1045),
+	     xycoords='axes points', size=40, ha='left', va='bottom', color='black')
 
-# Plot previous FRB events
-ax.scatter(width, flux, alpha=0.70, label = 'Previous FRB events', s=200)
+# Scatter plot
+plt.scatter(width, flux, c=dm, s=500, alpha=0.7, edgecolor='black', linewidth=2, cmap='plasma', zorder=10)
 
-# Enable legend
-ax.legend()
+# Set colorbar
+cbar = plt.colorbar()
+cbar.set_label(r'$\mathrm{Dispersion \ Measure \ }\Bigg[\mathrm{pc \ cm}^{-3}\Bigg]$', fontsize=52)
+cbar.ax.tick_params(labelsize=42)
 
-# Enable grid
-ax.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.7)
+# Remove alpha colorbar component
+cbar.set_alpha(1)
+cbar.draw_all()
 
-# Add title
-ax.set_title('HeRTA | Minimum detectable FRB fluence', fontsize=44, y=1.01)
+# Set axis labels & figure title
+plt.xlabel(r'$\mathrm{Burst \ Width \ [ms]}$', fontsize=52)
+plt.ylabel(r'$\mathrm{Peak \ Flux \ Density \ [Jy]}$', fontsize=52)
+plt.title(r'$\mathrm{FRB \ Fluence \ Distribution}$', fontsize=72, y=1.01)
 
-# Set axis limits
-ax.set_xlim(10**-1,10**4)
-ax.set_ylim(10**-2,10**3)
-ax.set_xscale('log')
-ax.set_yscale('log')
+# Set log-log scaling
+plt.xscale('log')
+plt.yscale('log')
 
-# Set axis labels
-ax.set_xlabel('Pulse Width (ms)', fontsize=32)
-ax.set_ylabel('Peak Flux Density (Jy)', fontsize=32)
+# Set ylim
+plt.xlim(10**-1,10**4)
+plt.ylim(10**-2,10**3)
+
+# Set tick size
+plt.xticks(fontsize=42, y=-0.005)
+plt.yticks(fontsize=42)
 
 # Remove top and right border
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
 
-# Save plot to file
-plt.savefig('plot.png', bbox_inches='tight')
+plt.gca().xaxis.set_tick_params(top='off',which='both')
+plt.gca().yaxis.set_tick_params(right='off',which='both')
+
+plt.tight_layout()
+
+# Save data to a scalable format
+plt.savefig('plot.png')#, format='svg')
